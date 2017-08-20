b28ff18ff83ea813f22033185f52a3a27b74bbc2
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-08-20 11:59:31 +0200

    more on passport

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
 const keys = require('../config/keys');
+const User = mongoose.model('User');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  user ? done(null, user) : null;
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +21,11 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refresh, profile) => {
-      console.log(accessToken);
-      console.log(refresh);
-      console.log('profile', profile.id);
+    async (accessToken, refresh, profile, done) => {
+      const userExist = await User.findOne({ googleId: profile.id });
+      !userExist
+        ? new User({ googleId: profile.id }).save()
+        : done(null, userExist);
     }
   )
 );
